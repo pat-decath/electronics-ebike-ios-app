3cf8d3f01618609d1fb56de0c21ed0cc04eea104
Patrick Rayet <prayet@partner.auchan.fr>
2021-10-05 15:32:20 +0200

    Initial Commit

---
+++ electronics-ebike-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  electronics-ebike-ios-app
+//
+//  Created by FromMacBookPRA on 05/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
